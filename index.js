bcc79a0a5ebc6725efc40e613c0dd1284322a590
Yeshwanth L M <66474973+yeshwanthlm@users.noreply.github.com>
2022-10-05 00:14:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const http = require('http');
-const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'A Monk in Cloud!\n'
   res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+}).listen(80);
