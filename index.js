00296651381b0617f18e5aa644fb98fae4bf1e06
Saurabh Singh <43520906+saurabhsinghaniyas@users.noreply.github.com>
2023-04-30 17:16:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('You are  in AWS  Cloud   TECHESSAY'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+}).listen(3000); //the server object listens on port 80
