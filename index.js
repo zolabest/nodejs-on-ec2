b96ba0ca44da3bb71f6bd95666f4a87ee285098a
Saurabh Singh <43520906+saurabhsinghaniyas@users.noreply.github.com>
2023-04-30 17:17:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('You are  in AWS  Cloud   TECHESSAY'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port 80
+}).listen(3000); //the server object listens on port 3000
