a7d161a8fd411ef05dd8d6e1bb781ff7dcc423f7
zolabest <93548545+zolabest@users.noreply.github.com>
2023-10-10 23:27:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. THis is TECHESSAY, Please share and Subscribe and like my Videos'); //write a response to the client
+  res.write('You are  in AWS  Cloud. THis is ZolaTech, Please share and Subscribe and like my Videos'); //write a response to the client
   res.end(); //end the response
-}).listen(4000); //the server object listens on port 4000
+}).listen(3000); //the server object listens on port 3000
